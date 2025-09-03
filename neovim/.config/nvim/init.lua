85fa5850f3f9ba7198d27dff76109c3910e834e9
Kieren Hinch <khinch-github@nylonmoon.com>
2025-09-03 19:28:37 +0100

    Configs for lazyvim with telescope plugin

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -24,7 +24,11 @@ vim.wo.number = true
 vim.wo.relativenumber = true
 
 vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
+
+-- lazy vim: https://lazy.folke.io/
+require("config.lazy")
